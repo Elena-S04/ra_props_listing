55181278aa835b28fe689d212a74e96bec46efaf
Elena Spirina <espirina@list.ru>
2023-10-15 12:22:24 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import Listing from './components/ListingFunc';
+import data from './data/etsy.json'
 import './App.css';
 
 function App() {
+  const list = data.map(item => { 
+    const { listing_id, url, MainImage, title, currency_code, price, quantity } = item
+    return {
+      listing_id,
+      url, 
+      MainImage, 
+      title, 
+      currency_code, 
+      price, 
+      quantity
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-content">
+      <Listing items={list}/>
     </div>
   );
 }
